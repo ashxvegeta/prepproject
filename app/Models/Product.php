f765d3ae3d4c613729d70a5d69507b81d0fc7be5
ashxvegeta <ashutoshroy453@gmail.com>
2026-06-22 22:03:51 +0530

    Addded product Relationship

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,7 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+
 class Product extends Model
 {
     //
+
+    protected $fillable = ['category_id', 'name', 'slug', 'description', 'price', 'stock', 'image', 'is_active'];
+
+    public function category():BelongsTo{
+        return $this->belongsTo(category::class);
+    }
+
+    public function cartItems():HasMany{
+        return $this->hasMany(cartItem::class);
+    }
+
+
+     public function orderItems():HasMany{
+        return $this->hasMany(orderItem::class);
+    }
+
 }
