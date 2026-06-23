2ed7c9ab5f53ce050f108c22820bd9d2e517573b
ashxvegeta <ashutoshroy453@gmail.com>
2026-06-23 21:43:38 +0530

    Cart and CartItem relationship Added

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Cart extends Model
 {
     //
+    protected $fillable =  ['user_id'];
+
+    // this cart belongs to thsi user
+    public function user():BelongsTo{
+
+       return $this->belongsTo(User::class);
+    }
+    //this cart has many items
+    public function item():HasMany{
+        return $this->hasMany(CartItem::class);
+    }
 }
