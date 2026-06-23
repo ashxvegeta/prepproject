997171d77582eeb7a6384c66dc11972696459703
ashxvegeta <ashutoshroy453@gmail.com>
2026-06-23 22:11:55 +0530

    OrderItem and Order Relationship Added

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,8 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Order extends Model
 {
     //
+
+    protected $fillable = ['user_id', 'total', 'status', 'shipping_address'];
+
+    public function user():BelongsTo{
+        return $this->belongsTo(User::class);
+    }
+
+    public function items():HasMany{
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function payment():HasOne{
+        return $this->hasOne(Payment::class);
+    }
+
+
 }
