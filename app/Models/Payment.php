16211dccff8e56fd155e48aa473e3292adac0375
ashxvegeta <ashutoshroy453@gmail.com>
2026-06-23 22:18:24 +0530

    Payment Relationship added

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Payment extends Model
 {
     //
+
+    protected $fillable = ['order_id', 'payment_method', 'transaction_id', 'amount', 'status'];
+
+    public function user():BelongsTo{
+        return $this->belongsto(Order::class);
+    }
 }
